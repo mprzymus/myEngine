d97892388a9e887b29347d1f67e335e8b867035a
mprzymus <pzrymus@gmail.com>
2019-09-16 16:48:23 +0200

    Color switch
    Add possibility to draw some quads red

--- Debugger.cpp
+++ Debugger.cpp
@@ -5,7 +5,22 @@
 
 void Debugger::addCollidable(std::shared_ptr<RectCollisionComponent> collidable)
 {
-	components.push_back(posData(collidable,collidable->getOwner().getPosition().getPosition()));
+	posData temp(pairData(collidable, collidable->getOwner().getPosition().getPosition()));
+	components.push_back(temp);
+}
+
+void Debugger::removeCollidable(std::shared_ptr<RectCollisionComponent> collidable)
+{
+	auto it = std::find(components.begin(), components.end(), posData(pairData(collidable, collidable->getOwner().getPosition().getPosition())));
+	if (it != components.end())
+		components.erase(it);
+}
+
+void Debugger::setSpecial(std::shared_ptr<RectCollisionComponent> collidable, bool special)
+{
+	auto it = std::find(components.begin(), components.end(), posData(pairData(collidable, collidable->getOwner().getPosition().getPosition())));
+	if (it != components.end())
+		it->isSpecial = special;
 }
 
 void Debugger::drawCollidableSquares()
@@ -13,20 +28,29 @@ void Debugger::drawCollidableSquares()
 	for (auto& collidable : components)
 	{
 		sf::VertexArray square(sf::LineStrip, 5);
+		sf::Color color;
 		
-		square[0].position = square[4].position = sf::Vector2f(collidable.first->getBounds().left, collidable.first->getBounds().top);
-		square[1].position = sf::Vector2f(collidable.first->getBounds().left+collidable.first->getBounds().width, collidable.first->getBounds().top);
-		square[2].position = sf::Vector2f(collidable.first->getBounds().left+collidable.first->getBounds().width, collidable.first->getBounds().top + collidable.first->getBounds().height);
-		square[3].position = sf::Vector2f(collidable.first->getBounds().left, collidable.first->getBounds().top + collidable.first->getBounds().height);
-		window.draw(square);
-		sf::VertexArray centre(sf::Points, 1);
-		centre[0].position = { collidable.first->getCentre().x, collidable.first->getCentre().y };
-		window.draw(centre);
+		square[0].position = square[4].position = sf::Vector2f(collidable.first()->getBounds().left, collidable.first()->getBounds().top);
+		square[1].position = sf::Vector2f(collidable.first()->getBounds().left+collidable.first()->getBounds().width, collidable.first()->getBounds().top);
+		square[2].position = sf::Vector2f(collidable.first()->getBounds().left+collidable.first()->getBounds().width, collidable.first()->getBounds().top + collidable.first()->getBounds().height);
+		square[3].position = sf::Vector2f(collidable.first()->getBounds().left, collidable.first()->getBounds().top + collidable.first()->getBounds().height);
 
-		if (loud && collidable.first->getOwner().getPosition().getPosition() != collidable.second)
-			std::cout << "Was:" << collidable.second.x << ", " << collidable.second.y << " is: "
-			<< collidable.first->getOwner().getPosition().getPosition().x << ", " 
-			<< collidable.first->getOwner().getPosition().getPosition().y << std::endl;
-		collidable.second = collidable.first->getOwner().getPosition().getPosition();
+		sf::VertexArray centre(sf::Points, 1);
+		centre[0].position = { collidable.first()->getCentre().x, collidable.first()->getCentre().y };
+
+		if (collidable.isSpecial) {
+			for (int i = 0; i < 4; i++)
+				square[i].color = sf::Color::Red;
+			centre[0].color = sf::Color::Red;
+			collidable.isSpecial = false;
+		}
+		window.draw(centre);
+		window.draw(square);
+
+		if (loud && collidable.first()->getOwner().getPosition().getPosition() != collidable.second())
+			std::cout << "Was:" << collidable.second().x << ", " << collidable.second().y << " is: "
+			<< collidable.first()->getOwner().getPosition().getPosition().x << ", " 
+			<< collidable.first()->getOwner().getPosition().getPosition().y << std::endl;
+		collidable.second() = collidable.first()->getOwner().getPosition().getPosition();
 	}
 }
--- Debugger.h
+++ Debugger.h
@@ -2,16 +2,28 @@
 #include <vector>
 #include "RectCollisionComponent.h"
 
-using posData = std::pair<std::shared_ptr<RectCollisionComponent>, sf::Vector2f>;
+using pairData = std::pair < std::shared_ptr<RectCollisionComponent>, sf::Vector2f>;
 
 class Debugger
 {
+	struct posData
+	{
+		std::pair<std::shared_ptr<RectCollisionComponent>, sf::Vector2f> data;
+		bool isSpecial;
+		bool operator== (const posData& right)const { return data.first == right.data.first; }
+		posData(pairData Data)
+			: data(Data), isSpecial(false) {}
+		std::shared_ptr<RectCollisionComponent>& first() { return data.first; }
+		sf::Vector2f& second() { return data.second; }
+	};
 	bool loud;
 	sf::RenderWindow& window;
 	std::vector<posData> components;
 public:
 	Debugger(sf::RenderWindow& window, bool loud = false) : window(window), loud(loud) {}
 	void addCollidable(std::shared_ptr<RectCollisionComponent> collidable);
+	void removeCollidable(std::shared_ptr<RectCollisionComponent> collidable);
 	void drawCollidableSquares();
+	void setSpecial(std::shared_ptr<RectCollisionComponent> collidable, bool special);
 };
 
